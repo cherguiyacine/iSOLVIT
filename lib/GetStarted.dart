f5f194bd15983bc506694ea3b88909d9d0bed27a
yacine chergui <hb_chergui@esi.dz>
2020-06-09 02:21:50 +0100

    add get started page

--- lib/GetStarted.dart
+++ lib/GetStarted.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_overboard/flutter_overboard.dart';
 
 class GetStarted extends StatefulWidget {
   @override
@@ -9,23 +10,53 @@ class GetStarted extends StatefulWidget {
   }
 }
 
-class GetStartedState extends State<GetStarted> {
-//  MapState(this.initx, this.inity);
-  initState() {
+class GetStartedState extends State<GetStarted> with TickerProviderStateMixin {
+  final GlobalKey<ScaffoldState> _globalKey = new GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
     super.initState();
   }
 
-  //Completer<GoogleMapController> _controller = Completer();
-
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
-    // TODO: implement build
-    return MaterialApp(
-      home: SafeArea(
-        child: Scaffold(body: Container()),
+    return Scaffold(
+      key: _globalKey,
+      body: OverBoard(
+        pages: pages,
+        showBullets: true,
+        skipCallback: () {
+          _globalKey.currentState.showSnackBar(SnackBar(
+            content: Text("Skip clicked"),
+          ));
+        },
+        finishCallback: () {
+          _globalKey.currentState.showSnackBar(SnackBar(
+            content: Text("Finish clicked"),
+          ));
+        },
       ),
     );
   }
+
+  final pages = [
+    PageModel(
+        color: const Color(0xfff0DBBD6),
+        imageAssetPath: 'images/test2.png',
+        title: 'Get started',
+        body: 'Share your ideas with the team Share your ideas',
+        doAnimateImage: true),
+    PageModel(
+        color: const Color(0xfff11A0B6),
+        imageAssetPath: 'images/test3.png',
+        title: 'Screen 2',
+        body: 'Connect with the people from different places',
+        doAnimateImage: true),
+    PageModel(
+        color: const Color(0xfff157DE6),
+        imageAssetPath: 'images/tets.png',
+        title: 'Screen 3',
+        body: 'A conceptual design for on boarding screens for mobile apps',
+        doAnimateImage: true),
+  ];
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:iSOLVIT/GetStarted.dart';
 
+import 'LunchPage.dart';
 import 'LunchPage.dart';
 
 /*void main() => runApp(MaterialApp(
