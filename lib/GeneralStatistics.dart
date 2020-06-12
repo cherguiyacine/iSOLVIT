7bcef2fe697aeb5c7af3b40d6cd0cd447d984652
yacine chergui <hb_chergui@esi.dz>
2020-06-12 13:22:40 +0100

    add generalStatisticPage

---
+++ lib/GeneralStatistics.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:iSOLVIT/GetStarted.dart';
+
+class GeneralStatisticsPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return GeneralStatisticsPageState();
+  }
+}
+
+class GeneralStatisticsPageState extends State<GeneralStatisticsPage> {
+  initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    // TODO: implement build
+    return Scaffold(
+      backgroundColor: Color(0xff329cef),
+      body: new Stack(
+        children: <Widget>[
+          new Container(
+            decoration: new BoxDecoration(
+                image: new DecorationImage(
+              image: new AssetImage("images/start1.PNG"),
+              fit: BoxFit.fill,
+            )),
+          ),
+        ],
+      ),
+    );
+  }
+}
